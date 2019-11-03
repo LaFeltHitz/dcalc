be8eb4ebc69b19a5161c4e3959769851de98ece2
Derel Hicks <derelhicks@gmail.com>
2019-11-03 00:11:39 -0400

    added cmd parse lib

--- cmd/root.go
+++ cmd/root.go
@@ -10,16 +10,24 @@ const (
 	_1m float64 = 60000
 )
 
+var tempo float64
+
 var rootCmd = &cobra.Command{
 	Use:   "dcalc",
 	Short: "Delay Calculator calculates note delay based on a given tempo",
 	Run: func(cmd *cobra.Command, args []string) {
-
-	},
+		
+	}
 }
 
 func Execute() error {
 	return rootCmd.Execute()
+}
+
+func init() {
+	var flagName = "t"
+	rootCmd.Flags().Float64Var(&tempo, flagName, 120, "beat tempo")
+	rootCmd.MarkFlagRequired(flagName)
 }
 
 func calculateDelay(tempo *float64) float64 {
